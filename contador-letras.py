ced2fa971c5f9c5d66bf9dfebc779d7177e4f735
BiancaDV96 <devincenzibianca@gmail.com>
2025-01-31 19:59:44 -0300

    Update contador-letras.py

--- contador-letras.py
+++ contador-letras.py
@@ -1,14 +1,10 @@
 #Paso 1: Solicitar entrada al usuario
 
-from faulthandler import cancel_dump_traceback_later
-
-
 palabra_ingresada = input("Por favor ingrese una palabra")
 
 #Paso 2: Contar la cantidad de letras
 
 cant_letras = len(palabra_ingresada)
-
 
 #Paso 3: Mostrar al usuario el resultado
 
